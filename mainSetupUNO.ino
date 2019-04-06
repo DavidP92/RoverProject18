e54b84c68ebc54e01c914f8dc994a9ead86d6bd7
MaSanto Raffiki <Davidpaez1006@gmail.com>
2019-04-06 15:52:47 -0600

    Update mainSetupUNO.ino

--- mainSetupUNO.ino
+++ mainSetupUNO.ino
@@ -1,113 +1,213 @@
+#include <Servo.h>
 #include <Wire.h>
 #include <LIDARLite.h>
-#include <Servo.h>
+#include "SparkFunIMU.h"
+#include "SparkFunLSM303C.h"
+#include "LSM303CTypes.h"
 
-#define topServo A0
-#define bottServo A2
-Servo topS;
-Servo botS;
+Servo panServo;
+Servo tiltServo;
 LIDARLite Laser;
+LSM303C myIMU;
 
 int xDistance = 0; 
-
-int posTT = 80;   //Top Servo Position
-int posB = 0;     //Bottom Servo Position
-int posBB = 180;  //Bottom Floor Sweep End Position
-int posBL = 0;    //Bottom Left Stop Position
-int posBM = 120;   //Bottom Midpoint Stop Position
-int posTM = 80;   //Top Midpoint Stop Position
-int scan = 2;    //Number of Laser Scans
+int posPan = 0;    
+int posTilt = 0;
+int scan = 1;
+float headingDegrees;
 
 unsigned long start;      //Timer
 unsigned long timer_1;    //Timer ---> Laser Function
 unsigned long timer_2;  //Timer----> Motor Command
-
 uint8_t delay_1 = 100;
 uint8_t delay_2 = 3000;
 
-void panTiltSetup() {
-  topS.attach(topServo);
-  botS.attach(bottServo);
-  Laser.begin(0, true);
-  Laser.configure(0);
-  Serial.println("Laser Setup Completed");
-}
 void sendCommand(byte dir) {
   Wire.beginTransmission(69);          // transmit to device #8               // sends five bytes
   Wire.write(dir);                     // sends one byte
   Wire.endTransmission();              // stop
   delay(1000);
+  Serial.print("The direction value being sent:   ");
   Serial.println(dir);
 }
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting Setup..............");
-  panTiltSetup();
-  //Wire.begin();
-  timer_1 = millis();
-  timer_2 = timer_1;
-  Serial.println("Setup has been completed....");
-}
-void laserRead(int timer, int i){
-  for (int j = 0; i > j;j++){
-    Serial.print("Object Distance:    ");
-    xDistance = Laser.distance();
-    Serial.println(xDistance);
-    delay(100);
+
+void panMotion(byte dir){
+  if(dir == 0){
+      Serial.println("Forward Pan Motion");
+      for (posPan = 0; posPan <= 160; posPan += 12) { 
+        panServo.write(posPan);
+        Serial.println(posPan);   
+        laserRead();                     
+      }
   }
+  else if ( dir ==1){
+      Serial.println("Backward Pan Motion");
+        for (posPan = 140; posPan >= 0; posPan -= 12) { 
+        panServo.write(posPan);  
+        Serial.println(posPan);           
+        laserRead();                    
+      }
+  }
+    else if(dir==2){
+      posPan = 115;      panServo.write(posPan);
+      laserRead();
+    }
+    
+    else if(dir==3){
+      posPan = 0;
+      panServo.write(posPan);
+      laserRead();
+      if(xDistance < 40){
+        sendCommand(4);
+        sendCommand(2);
+      }
+      else{
+        sendCommand(0);
+      }
+    }
+  }
+void tiltMotion(byte dir){
+    if(dir == 0){
+      Serial.println("Forward Tilt Motion");
+      for (posTilt = 0; posTilt <= 25; posTilt += 5) { 
+        tiltServo.write(posTilt);
+        Serial.println(posTilt); 
+        laserRead();   
+        delay(100);                  
+      }
+    }
+    else if(dir == 1){
+      Serial.println("Backward Tilt Motion");
+      for (posTilt = 25; posTilt >= 0; posTilt -= 2) { 
+        tiltServo.write(posTilt);
+        Serial.println(posTilt);            
+        laserRead();
+        delay(100);                   
+      }
+    }
+}
+void readCompass(int item)
+{
+  switch (item)
+  {
+      float value;
+      float heading;
+    case 0:
+      value = myIMU.readAccelX();
+      // Assume that if X is not activated then none are (poor assumption, but demo)
+      if ( !isnan(value) )
+      {
+        Serial.print("\nAccelerometer:\n X = ");
+        Serial.println(value, 4);
+        Serial.print(" Y = ");
+        Serial.println(myIMU.readAccelY(), 4);
+        Serial.print(" Z = ");
+        Serial.println(myIMU.readAccelZ(), 4);
+      }
+      break;
+    case 1:
+      value = myIMU.readGyroX();
+      // Not supported by hardware, so will return NAN
+      if ( !isnan(value) )
+      {
+        Serial.print(F("\nGyroscope:\n X = "));
+        Serial.println(value, 4);
+        Serial.print(" Y = ");
+        Serial.println(myIMU.readGyroY(), 4);
+        Serial.print(" Z = ");
+        Serial.println(myIMU.readGyroZ(), 4);
+      }
+      break;
+    case 2:
+      value = myIMU.readMagX();
+      if ( !isnan(value) )
+      {
+        Serial.print("\nMagnetometer:\n X = ");
+        Serial.println(value, 4);
+        Serial.print(" Y = ");
+        Serial.println(myIMU.readMagY(), 4);
+        Serial.print(" Z = ");
+        Serial.println(myIMU.readMagZ(), 4);
+      }
+      break;
+    case 3:
+      value = myIMU.readTempC();
+      if ( !isnan(value) )
+      {
+        Serial.print("\nThermometer:\n");
+        Serial.print(" Degrees C = ");
+        Serial.println(value, 4);
+        Serial.print(" Degrees F = ");
+        Serial.println(myIMU.readTempF(), 4);
+      }
+      break;
+    case 4:
+      heading = atan2(myIMU.readMagY(), myIMU.readMagX());
+      if (heading < 0)
+        heading += 2 * PI;
+      headingDegrees = heading * 180 / M_PI;
+      Serial.print("Theta: ");
+      Serial.println(headingDegrees);
+      break;
+    default:
+      Serial.println(F("Failed to enter in a vaild option."));
+  }
+}
+void laserRead(){
+  for (int j = 0; scan > j;j++){
+    Serial.print("Object Distance:    ");
+    Serial.println(Laser.distance());
+    xDistance = Laser.distance();
+    delay(24);
+  }
+}
+void setup(){
+  Serial.begin(115200);
+  panServo.attach(A0);
+  tiltServo.attach(A2);
+  Laser.begin(0, true);
+  Laser.configure(0);
+  Serial.println("Laser Setup Completed");
+  if (myIMU.begin(
+        MODE_I2C,
+        MAG_DO_40_Hz,
+        MAG_FS_16_Ga,
+        MAG_BDU_ENABLE,
+        MAG_OMXY_HIGH_PERFORMANCE,
+        MAG_OMZ_HIGH_PERFORMANCE,
+        MAG_MD_CONTINUOUS,
+        ACC_FS_2g,
+        ACC_BDU_ENABLE,
+        ACC_X_ENABLE | ACC_Y_ENABLE | ACC_Z_ENABLE,
+        ACC_ODR_100_Hz
+      ) != IMU_SUCCESS)
+  {
+    Serial.println(F("Compass/Magnet/Accel Failed setting up."));
+    while (1);
+  }
+  Serial.println("Setup Configurations is completed.......");
+  timer_1 = millis();
 }
 
 void loop() {
-  midStop(100, posBM,scan);
-  if(xDistance < 50){
-    sendCommand(5);
+   int i = 0;
+   start = millis();
+   panMotion(3);
+   tiltMotion(0);
+   if(xDistance < 50){
+    sendCommand(4);
     sendCommand(2);
-  }
-  else{
-    sendCommand(1);
-  }
-}
-void sweep(int timer, int posTT, int scan) {
-  for (posTT; posTT <= 160; posTT += 5) {
-//    Serial.print("(1)The position of Top is : ");
-//    Serial.print(posTT);
-//    Serial.print("  Movement Function: ");
-    Serial.println("Sweep Function");
-    laserRead(timer,scan);
-    topS.write(posTT);
-    delay(timer);
-  }
-}
-
-void floorSweep(int timer, int posB, int scan) {
-  for (posB; posB <= 185; posB += 5) {
-//    Serial.print("(3)The position of Top is : ");
-//    Serial.print(posB);
-//    Serial.print("  Movement Function: ");
-    Serial.println("Floor Sweep Function");
-    laserRead(timer,scan);
-    botS.write(posB);
-    delay(timer);
-  }
-}
-
-void midStop(int timer, int posBM, int scan) {
-//  Serial.print("(5)The position of Top is : ");
-//  Serial.print(posBM);
-//  Serial.print("  Movement Function: ");
-  Serial.println("Mid-Stop Function.!");
-  laserRead(timer,scan);
-  botS.write(posBM);
-  topS.write(posTM);
-  delay(timer);
-}
-
-void botleftStop(int posBL, int timer, int scan){
-//  Serial.print("(6)The position of Top is : ");
-//  Serial.print(posBL);
-//  Serial.print("  Movement Function: ");
-  Serial.println("Bottom Left Stop");
-  laserRead(timer,scan);
-  botS.write(posBL);
-  delay(timer);
+    i = i + 1;
+    if(xDistance < 40){
+      sendCommand(3);
+      sendCommand(3);
+      i = i + 1;
+    }
+    else{
+      sendCommand(0);
+    }
+      if (xDistance < 40){
+        sendCommand(3); 
+        }
+   }
 }
